b61524eacc5c77e7daa4ea3868f1864da0863c8d
jaysonpye <jaysonpye@hotmail.com>
2025-10-17 10:44:15 +0900

    skeleton

---
+++ wp-content/themes/wellpeak-site/front-page.php
@@ -0,0 +1,11 @@
+// Doing things as template parts is probably smart so we can work on them separately.
+
+<?php get_header(); ?>
+
+<?php get_template_part("template-parts/hero"); ?>
+<?php get_template_part("template-parts/features"); ?>
+<?php get_template_part("template-parts/cta"); ?>
+<?php get_template_part("template-parts/faq"); ?>
+<?php get_template_part("template-parts/news"); ?>
+
+<?php get_footer(); ?>
