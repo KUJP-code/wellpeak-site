a28ed93e169c960e9cbd5e2c10b544a7964c5bd8
jaysonpye <jaysonpye@hotmail.com>
2025-10-27 22:27:56 +0900

    add index base

--- wp-content/themes/wellpeak-site/index.php
+++ wp-content/themes/wellpeak-site/index.php
@@ -1,9 +1,26 @@
 <?php
+/**
+ * The main template file
+ *
+ * This is the fallback for all queries that don’t have a page, this shouldn't really be called over 4.4 unless there is a mistake in the front end
+ */
+
 get_header(); ?>
-<!--
-TODO: fallback page, we should style some easy content here so they can still just access through header/footer
-a-->
-<main class="container">
-  <p>This is a fallback page, something went wrong!</p>
+
+<main id="main" class="site-main container">
+  <?php if (have_posts()):
+      // loop posts/pages if available
+      while (have_posts()):
+          the_post();
+          get_template_part("template-parts/content", get_post_type());
+      endwhile;
+
+      the_posts_navigation();
+      echo '<p style="text-align:center; margin:2rem 0;">コンテンツがありません。</p>';
+      // no content fallback
+  else:
+      get_template_part("404");
+  endif; ?>
 </main>
-<?php get_footer();
+
+<?php get_footer(); ?>
