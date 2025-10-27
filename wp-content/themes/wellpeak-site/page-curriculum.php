eb64ff1dfb16993a2ebf1e08454ca91803b05ed6
jaysonpye <jaysonpye@hotmail.com>
2025-10-27 22:28:39 +0900

    curriculum structure

--- wp-content/themes/wellpeak-site/page-curriculum.php
+++ wp-content/themes/wellpeak-site/page-curriculum.php
@@ -6,10 +6,16 @@
 get_header(); ?>
 
 <main id="curriculum" class="curriculum-page">
-  <?php get_template_part("template-parts/company/intro"); ?>
-  <?php get_template_part("template-parts/company/executives"); ?>
-  <?php get_template_part("template-parts/company/profile"); ?>
+<?php get_template_part("template-parts/page-banner", null, [
+    "title" => get_the_title(),
+    "image" => get_stylesheet_directory_uri() . "/images/hero-splash.jpg",
+]); ?>
+  <?php get_template_part("template-parts/curriculum/intro"); ?>
+  <?php get_template_part("template-parts/curriculum/about"); ?>
+  <?php get_template_part("template-parts/cta"); ?>
+  <?php get_template_part("template-parts/curriculum/ku-msg"); ?>
+  <?php get_template_part("template-parts/curriculum/course"); ?>
 </main>
 
 <?php get_footer();
-// set route as  company in wordpress for consistency.
+// set route as  curriculum in wp-admin for this page
