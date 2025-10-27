3f094bf185d5ed2cce983580dcb1d597463e8623
jaysonpye <jaysonpye@hotmail.com>
2025-10-27 22:28:33 +0900

    course structure

--- wp-content/themes/wellpeak-site/page-course.php
+++ wp-content/themes/wellpeak-site/page-course.php
@@ -6,6 +6,10 @@
 get_header(); ?>
 
 <main id="course" class="course-page">
+    <?php get_template_part("template-parts/page-banner", null, [
+        "title" => get_the_title(),
+        "image" => get_stylesheet_directory_uri() . "/images/hero-splash.jpg",
+    ]); ?>
   <?php get_template_part("template-parts/company/intro"); ?>
   <?php get_template_part("template-parts/company/executives"); ?>
   <?php get_template_part("template-parts/company/profile"); ?>
