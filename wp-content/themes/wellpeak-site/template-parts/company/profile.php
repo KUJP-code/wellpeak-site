6f0110b5eee362a54a305b6b5ee6c8ae47fcb520
jaysonpye <jaysonpye@hotmail.com>
2025-10-29 13:36:11 +0900

    company updates

--- wp-content/themes/wellpeak-site/template-parts/company/profile.php
+++ wp-content/themes/wellpeak-site/template-parts/company/profile.php
@@ -19,11 +19,12 @@
 
     <div class="profile-card" role="table" aria-label="会社概要">
         <dl>
-          <div><dt>商号</dt><dd>株式会社何々</dd></div>
-          <div><dt>所在地</dt><dd>東京都何々区何々町</dd></div>
-          <div><dt>設立</dt><dd>2020年1月1日</dd></div>
+          <div><dt>商号</dt><dd>WELLPEAK KIDS ENGLISH SCHOOL （セルテック株式会社）</dd></div>
+          <div><dt>所在地</dt><dd>〒151-0064 東京都渋谷区上原3丁目1−16 （WELLPEAK 代々木上原校）</dd></div>
+          <div><dt>設立</dt><dd>2026年2月</dd></div>
+          <div><dt>事業責任者</dt><dd>佐藤 健二</dd></div>
           <div><dt>役員一覧</dt><dd>代表取締役 何々太郎</dd></div>
-          <div><dt>事業内容</dt><dd>英会話スクール運営、教育コンサルティング</dd></div>
+          <div><dt>事業内容</dt><dd>アフタースクールの運営（幼児/小学生）イベント事業、キャンプ事業</dd></div>
         </dl>
     </div>
   </div>
